6bc3cad74889a2f92b372512e56bc31eda505866
sinsombat <sinsombat.j@gmail.com>
2018-07-09 17:03:31 +0700

    init

--- razzle.config.js
+++ razzle.config.js
@@ -1,5 +1,19 @@
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   modify: (config, { target, dev }, webpack) => {
+    config.externals = target === 'node' ? [
+      nodeExternals({
+        whitelist: [
+          dev ? 'webpack/hot/poll?300' : null,
+          /\.(eot|woff|woff2|ttf|otf)$/,
+          /\.(svg|png|jpg|jpeg|gif|ico)$/,
+          /\.(mp4|mp3|ogg|swf|webp)$/,
+          /\.(css|scss|sass|sss|less)$/,
+          /^my-untranspiled-package/
+        ].filter(Boolean),
+      }),
+    ] : [];
     const targetName = target === 'node' ? 'server' : 'client'
     return require(`./webpack/${targetName}`)(config, webpack)
   },
--- webpack/client.js
+++ webpack/client.js
@@ -1,48 +1,48 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const antdTheme = require('../src/theme') // <- Include variables to override antd theme
 
-const extractLess = new ExtractTextPlugin({
+const extractLess = new MiniCssExtractPlugin({
   filename: 'static/css/[name].[contenthash].css',
   disable: process.env.NODE_ENV === 'development', // disable this during development
 })
 
-module.exports = (config, webpack) => ({
-  ...config,
-  module: {
-    ...config.module,
-    rules: [
-      ...config.module.rules,
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          plugins: [['import', { libraryName: 'antd', style: true }]],
+module.exports = (config, webpack) => {
+  return {
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            plugins: [['import', { libraryName: 'antd', style: true }]],
+          },
         },
-      },
-      {
-        test: /\.less$/,
-        // use the ExtractTextPlugin instance
-        use: extractLess.extract({
-          use: [
-            {
-              loader: 'css-loader',
-            },
-            {
-              loader: 'less-loader',
-              options: {
-                modifyVars: antdTheme,
-              },
-            },
-          ],
-          // use style-loader in development
-          fallback: 'style-loader',
-        }),
-      },
+        {
+          test: /\.less$/,
+          use: [{
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              modifyVars: antdTheme
+            }
+          }]
+        },
+      ],
+    },
+    plugins: [
+      ...config.plugins,
+      new webpack.DefinePlugin({
+        'process.env.BROWSER': JSON.stringify(false)
+      }),
+      extractLess, // <- Add the ExtractTextPlugin instance here
     ],
-  },
-  plugins: [
-    ...config.plugins,
-    extractLess, // <- Add the ExtractTextPlugin instance here
-  ],
-})
+  }
+}
